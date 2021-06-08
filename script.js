9c5e77dc3bfc898c02a093f1c700c3c9c7a52722
kmre <karla.romero7@gmail.com>
2021-06-08 12:55:58 -0400

    first commit for develop

--- script.js
+++ script.js
@@ -1,13 +1,51 @@
 // Assignment code here
 
+//<Test>
+function generatePassword() {
 
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+  var password = "test4";
+  console.log(genPassword.passLenght);
+
+  return password; //Returns value to the writePassword fn for var password
+
+}
+
+//Input for length, lower case, upper case, number and special characters
+
+
+//Arrays or objects? for each of the selections
+
+
+//Loops to select and display random items from selected options
+
+
+
+//Object for password's parameters
+var genPassword = {
+  passLenght: 8,
+  passLowerCase: true,
+  passUpperCase: true,
+  passNumber: true,
+  passSpecialChar: true
+
+};
+
+
+generatePassword(genPassword);
+
+
+///</Test>
+//////////////////////////////////////////////////////////////////
+/////////////////////Given Code///////////////////////////////////
+////////////////////////////////////////////////////////////////
+
+// Get references to the #generate element = button on html
+var generateBtn = document.querySelector("#generate"); 
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  var password = generatePassword(); //Runs fn generatePassword
+  var passwordText = document.querySelector("#password"); //pases passwordText.value to the text area in HTML
 
   passwordText.value = password;
 
